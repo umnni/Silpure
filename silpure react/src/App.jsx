cac2e9a62b416a81916931ad653d7ef4d2577a6c
umnni <manish@umnni.com>
2026-09-23 18:25:26 +0530

    Fix GitHub Pages router base path

--- silpure react/src/App.jsx
+++ silpure react/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
